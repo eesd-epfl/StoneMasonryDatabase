928b1c599fd0ba5cb8299521f7df271575fd2364
ploeliger <patrick.loeliger@gmail.com>
2021-11-02 14:22:45 +0100

    table split into thead and tbody

--- Simple_JS_dashboard/javascript/data_table.js
+++ Simple_JS_dashboard/javascript/data_table.js
@@ -36,10 +36,17 @@ function ProcessExcel(data) {
     let firstSheet = workbook.SheetNames[0];
 
     //Read all rows from First Sheet into an JSON array.
-    let excelRows = XLS.utils.sheet_to_row_object_array(workbook.Sheets[firstSheet],{header:1});
+    let excelObject = XLS.utils.sheet_to_row_object_array(workbook.Sheets[firstSheet],{header:1});
 
-    console.log(excelRows);
-    
+    //Remove empty rows from array:
+    let finalObject = [];
+    for (let i = 0; i<excelObject.length; i++){
+        if(excelObject[i].length!=0){
+            finalObject.push(excelObject[i]);
+        }
+    }
+    createTable(finalObject);
+
     // //Create a HTML Table element.
     // let table = document.createElement("table");
 
@@ -80,4 +87,56 @@ function ProcessExcel(data) {
     // datatable.appendChild(table);
 };
 
-Display()
+function createTable(excelArray){
+    //Create Data table element and give it a class name
+    const dataTable = document.getElementById('data-table');
+    const tbl = document.createElement('table');
+    tbl.className = "ui collapsing table";
+
+    //Add tHead and tBody to the table
+    const header = tbl.createTHead();
+    const tblBody = tbl.createTBody();
+
+    const headerRow = header.insertRow(0);
+
+    //Empty array to be filled in loop for List.JS
+    let nameValues = [];
+
+    for (let i = 0; i < excelArray.length; i++) {
+        let tr;
+        if(i != 0){
+            tr = tblBody.insertRow();
+        }
+
+        for (let j = 0; j < excelArray[i].length; j++) {
+        if (i === excelArray.length-1 && j === excelArray[i].length-1) {
+            break;
+        } else {
+            if(i == 0){
+                //Add headers to array for List.JS
+                nameValues.push(excelArray[i][j]);
+                const cell = headerRow.insertCell(j);
+                cell.innerHTML = excelArray[i][j];
+                //create header row
+                // const th = tr.insertCell();
+                // th.appendChild(document.createTextNode)
+
+            } else {
+                const td = tr.insertCell();
+                td.className=excelArray[0][j];
+                td.appendChild(document.createTextNode(excelArray[i][j]));
+            }
+        }
+        }
+    }
+    const tBody = tbl.getElementsByTagName('tbody')[0];
+    tBody.className = 'list';
+
+    let options= {
+        valueNames: nameValues
+    };
+    dataTable.appendChild(tbl);
+    let tableList = new List('data-table',options);
+}
+
+Display();
